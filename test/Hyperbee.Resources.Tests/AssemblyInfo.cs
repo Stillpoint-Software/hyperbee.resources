70e2fe7e095f99b3682f03eeb6f54a0d0204d4e0
annette.findley <annette.varndell@wagglebee.net>
2025-12-18 14:08:59 -0500

    Centralized package versions and updated format.yml

---
+++ test/Hyperbee.Resources.Tests/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// Explicitly enable test parallelization at method level
+// This allows multiple test methods to run in parallel for faster execution
+// Safe for resource loading tests that don't share mutable state
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
